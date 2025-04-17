f06d6cb538664ab83286043b9c4376e5694ed7ec
8HANGLIU8 <8.h.a.n.g.l.i.u.8@gmail.com>
2025-04-17 11:44:22 -0400

    pm

--- possible_moves.cpp
+++ possible_moves.cpp
@@ -63,6 +63,10 @@ public:
     }
 };
 
+
+//---------------------OK-----------------------//
+
+
 struct Move {
     int fromRow, fromCol, toRow, toCol;
     Move(int fr, int fc, int tr, int tc) : fromRow(fr), fromCol(fc), toRow(tr), toCol(tc) {}
