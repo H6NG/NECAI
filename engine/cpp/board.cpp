810a0de575447b5958bb0e1486b94da9468526ed
H6NG <8.h.a.n.g.l.i.u.8@gmail.com>
2026-03-18 18:46:33 -0400

    adding OOP to my chess ai

---
+++ engine/cpp/board.cpp
@@ -0,0 +1,27 @@
+#include "board.h"
+#include <sstream> 
+
+Board::Board(){
+
+}
+
+void Board::load_fen(std::string fen){
+
+
+}
+bool Board::is_white_turn(){
+
+}
+
+void Board::parse_pieces(std::string board_part){
+
+}
+void Board::parse_turn(std::string turn_part){
+
+}
+void Board::parse_castling(std::string castle_part){
+
+}
+void Board::parse_en_passant(std::string ep_part){
+
+}
--- engine/cpp/board.h
+++ engine/cpp/board.h
@@ -3,10 +3,19 @@
 #include <array>
 
 
+enum Piece {
+    EMPTY = 0,
+    WHITE_PAWN = 1, WHITE_KNIGHT = 2, WHITE_BISHOP = 3,
+    WHITE_ROOK = 4, WHITE_QUEEN = 5,  WHITE_KING = 6,
+    BLACK_PAWN = 7, BLACK_KNIGHT = 8, BLACK_BISHOP = 9,
+    BLACK_ROOK = 10, BLACK_QUEEN = 11, BLACK_KING = 12
+};
+
 class Board {
 
     public: 
 
+        Board(); //it's own constructor I've decided to do OOP 
         void load_fen(std::string fen); 
         bool is_white_turn(); 
 
@@ -21,9 +30,9 @@ class Board {
         int en_passant; 
 
         //helper func 
-        void _parse_pieces(std::string board_part); 
-        void _parse_turn(std::string turn_part); 
-        void _parse_castling(std::string castle_part); 
-        void _parse_en_passant(); 
+        void parse_pieces(std::string board_part); 
+        void parse_turn(std::string turn_part); 
+        void parse_castling(std::string castle_part); 
+        void parse_en_passant(std::string ep_part); 
 
 }; 
