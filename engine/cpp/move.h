aeff40de3d344f9d5dd7a8a4e8823b16338323f2
H6NG <8.h.a.n.g.l.i.u.8@gmail.com>
2026-03-19 04:16:34 -0400

    Piece Object instead of just numbers

--- engine/cpp/move.h
+++ engine/cpp/move.h
@@ -1,16 +1,17 @@
 #pragma once 
+#include "board.h"
 
 struct Move{
 
     int from;
     int to; 
-    int promotion; 
-    int captured; 
+    Piece promotion; 
+    Piece captured; 
     bool is_en_passant; 
     bool is_castling; 
 
     //The Move constructor
-    Move(int from, int to, int promotion, int captured, bool is_en_passant, bool is_castling) :
+    Move(int from, int to, Piece promotion = EMPTY, Piece captured = EMPTY, bool is_en_passant = false, bool is_castling = false) :
     from(from), to(to), promotion(promotion), captured(captured), is_castling(is_castling), is_en_passant(is_en_passant){}; 
 }; 
 
