50917428bd091fd83d41d8f52820932bffdec580
H6NG <8.h.a.n.g.l.i.u.8@gmail.com>
2026-03-22 13:03:54 -0400

    didn't make any progress there

--- engine/cpp/search.h
+++ engine/cpp/search.h
@@ -4,14 +4,18 @@
 #include "eval.h"
 
 class Search {
-public:
-    Search(Board& board);
-    Move best_move(int depth);
 
-private:
-    Board& board;
-    MoveGenerator generator;
-    Eval eval;
+    public:
 
-    int negamax(int depth, int alpha, int beta);
+        Search(Board& board);
+        Move best_move(int depth);
+
+    private:
+
+        Board& board;
+        MoveGenerator generator;
+        Eval eval;
+
+        int negamax(int depth, int alpha, int beta);
+        
 };
